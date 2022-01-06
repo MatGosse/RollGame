5b224ffc88c22b0d66ef4d20e88e56bb5a977b9f
mat <mathieugosse13@gmail.com>
2022-01-06 14:47:17 +0100

    création fonction lancement de dés

--- src/game.js
+++ src/game.js
@@ -1,14 +1,31 @@
-function init(){
-    switchplayer(1);
-}
-function switchplayer(activeplayer){
+var activeplayer = 1; 
+
+function play(){
     if(activeplayer===1){
-        activeplayer++
-    }else{
-        activeplayer--
+        var curentscorescreen = document.querySelector('input[name="current_score_player_one"]');
+        var curentscore = parseInt(curentscorescreen.value);
+    }else {
+        var curentscorescreen = document.querySelector('input[name="current_score_player_two"]');
+        var curentscore = parseInt(curentscorescreen.value);
     }
-    play(activeplayer);
+    var scoreRoll = Math.floor(Math.random()*6)+1;
+    if(scoreRoll===1){
+        alert('');
+        curentscore=0;
+        curentscorescreen.value = 0;
+        switchplayer();
+    }else{
+        curentscore += scoreRoll;
+        curentscorescreen.value = curentscore; 
+    }
 }
-function play(activeplayer){
-    let currentScore = 0;
+
+function switchplayer(){
+    if(activeplayer===1){
+        activeplayer++;
+    }else{
+        activeplayer--;
+    }
+    console.log('le joueur actif est' + activeplayer);
+
 }
