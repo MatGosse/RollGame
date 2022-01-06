fc87ad08d1b099603edfc404ad60a33782362d50
mat <mathieugosse13@gmail.com>
2022-01-06 13:52:18 +0100

    ajout fonction changer joueur

---
+++ src/game.js
@@ -0,0 +1,14 @@
+function init(){
+    switchplayer(1);
+}
+function switchplayer(activeplayer){
+    if(activeplayer===1){
+        activeplayer++
+    }else{
+        activeplayer--
+    }
+    play(activeplayer);
+}
+function play(activeplayer){
+    let currentScore = 0;
+}
